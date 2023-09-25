bd70face2f43a209f7466cad12b7958158032ae4
Slađan Stevanović <sladjan.stevanovic05@gmail.com>
2023-09-25 15:27:13 +0200

    Vercel CORS 5

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -2,13 +2,24 @@ import { createTRPCContext } from '@/data/trpc/context';
 import { trpcRouter } from '@/data/trpc/routers/main';
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 
-const handler = (request: Request) => {
+async function trpcHandler(request: Request) {
     return fetchRequestHandler({
         endpoint: '/api/trpc',
         req: request,
         router: trpcRouter,
         createContext: async (options) => await createTRPCContext()
     });
-};
+}
+
+async function handler(request: Request) {
+    const response = await trpcHandler(request);
+
+    response.headers.append('Access-Control-Allow-Origin', '*');
+    response.headers.append('Access-Control-Request-Method', '*');
+    response.headers.append('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    response.headers.append('Access-Control-Allow-Headers', '*');
+
+    return response;
+}
 
 export { handler as GET, handler as POST };
