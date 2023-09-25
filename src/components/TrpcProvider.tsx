de790348601be587f5b6f04d26694f9e30b25ffb
Slađan Stevanović <sladjan.stevanovic05@gmail.com>
2023-09-25 16:19:45 +0200

    Vercel CORS 12

--- src/components/TrpcProvider.tsx
+++ src/components/TrpcProvider.tsx
@@ -19,7 +19,7 @@ export default function TrpcProvider({ children }: { children: ReactNode }) {
     );
 
     const url = process.env.NEXT_PUBLIC_VERCEL_URL
-        ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+        ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
         : 'http://localhost:3000/api/trpc/';
 
     const [trpcClient] = useState(() =>
