5b99fa90f87580ce1b7dd727635dbd8135919cf4
Slađan Stevanović <sladjan.stevanovic05@gmail.com>
2023-09-25 17:18:14 +0200

    Vercel CORS 14

--- next.config.js
+++ next.config.js
@@ -21,7 +21,10 @@ module.exports = {
                 source: '/api/:path*',
                 headers: [
                     { key: 'Access-Control-Allow-Credentials', value: 'true' },
-                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: 'https://soundhub-three.vercel.app/'
+                    },
                     {
                         key: 'Access-Control-Allow-Methods',
                         value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT'
--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -10,19 +10,6 @@ async function handler(request: Request) {
         createContext: async (options) => await createTRPCContext()
     });
 
-    /*
-    response.headers.set('Access-Control-Allow-Credentials', 'true');
-    response.headers.set('Access-Control-Allow-Origin', '*');
-    response.headers.set(
-        'Access-Control-Allow-Methods',
-        'GET,DELETE,PATCH,POST,PUT'
-    );
-    response.headers.set(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-    */
-
     return response;
 }
 
